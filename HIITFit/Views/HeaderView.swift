898c3679e846d14d7bbe3b67dca1f2e1b411f2c6
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-06-15 16:23:04 +0900

    Modify headerview.

--- HIITFit/Views/HeaderView.swift
+++ HIITFit/Views/HeaderView.swift
@@ -32,24 +32,32 @@
 
 import SwiftUI
 
-struct ExerciseView: View {
-    let index: Int
-    let videoNames = ["squat", "step-up", "burpee", "sun-salute"]
-    let exerciseNames = ["Squat", "Step Up", "Burpee", "Sun Salute"]
+struct HeaderView: View {
+    let exerciseName: String
     var body: some View {
         VStack {
-            HeaderView(exerciseName: exerciseNames[index])
-            Text("Video player")
-            Text("Timer")
-            Text("Start/Done button")
-            Text("Rating")
-            Text("History button")
+            Text(exerciseName)
+                .font(.largeTitle)
+            HStack {
+                Image(systemName: "1.circle")
+                Image(systemName: "2.circle")
+                Image(systemName: "3.circle")
+                Image(systemName: "4.circle")
+            }
+            .font(.title2)
         }
     }
 }
 
-struct ExerciseView_Previews: PreviewProvider {
+struct HeaderView_Previews: PreviewProvider {
     static var previews: some View {
-        ExerciseView(index: 0)
+        Group {
+            HeaderView(exerciseName: "Squat")
+                .previewLayout(.sizeThatFits)
+            HeaderView(exerciseName: "Squat")
+                .preferredColorScheme(.dark)
+                .environment(\.sizeCategory, .accessibilityLarge)
+                .previewLayout(.sizeThatFits)
+        }
     }
 }
