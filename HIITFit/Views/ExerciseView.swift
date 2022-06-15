07b414a6a97a9aa04b82917e9bd4e582cc0f2843
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-06-15 16:32:44 +0900

    Add video players.

--- HIITFit/Views/ExerciseView.swift
+++ HIITFit/Views/ExerciseView.swift
@@ -31,6 +31,7 @@
 /// THE SOFTWARE.
 
 import SwiftUI
+import AVKit
 
 struct ExerciseView: View {
     let index: Int
@@ -39,7 +40,12 @@ struct ExerciseView: View {
     var body: some View {
         VStack {
             HeaderView(exerciseName: exerciseNames[index])
-            Text("Video player")
+            if let url = Bundle.main.url(forResource: videoNames[index], withExtension: "mp4") {
+                VideoPlayer(player: AVPlayer(url: url))
+            } else {
+                Text("Couldn't find \(videoNames[index]).mp4")
+                    .foregroundColor(.red)
+            }
             Text("Timer")
             Text("Start/Done button")
             Text("Rating")
