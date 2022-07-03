96bdf8ac7467fb57faa15ba00f6ad6b7f655c4c9
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-07-04 08:59:29 +0900

    Give the custom button styles to the rating views.

--- HIITFit/Views/RatingView.swift
+++ HIITFit/Views/RatingView.swift
@@ -54,19 +54,22 @@ struct RatingView: View {
     var body: some View {
         HStack {
             ForEach(1..<maximumRating + 1) { index in
-                Image(systemName: "waveform.path.ecg")
-                    .foregroundColor(index > rating ? offColor : onColor)
-                    .onTapGesture {
-                        withAnimation {
-                            updateRating(index: index)
-                        }
+                Button {
+                    withAnimation {
+                        updateRating(index: index)
                     }
-                    .onAppear {
-                        convertRating()
-                    }
-                    .onChange(of: ratings) { _ in
-                        convertRating()
-                    }
+                } label: {
+                    Image(systemName: "waveform.path.ecg")
+                        .foregroundColor(index > rating ? offColor : onColor)
+                        .font(.body)
+                }
+                .buttonStyle(EmbossedButtonStyle(buttonShape: .round))
+                .onChange(of: ratings) { _ in
+                    convertRating()
+                }
+                .onAppear {
+                    convertRating()
+                }
             }
         }
         .font(.largeTitle)
