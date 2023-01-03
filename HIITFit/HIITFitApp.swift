79c5680666139407165fecd1b81cbe93cc8c4716
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-01-04 03:43:01 +0900

    Initial Commit

---
+++ HIITFit/HIITFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HIITFitApp.swift
+//  HIITFit
+//
+//  Created by Jinwook Kim on 2023/01/04.
+//
+
+import SwiftUI
+
+@main
+struct HIITFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
