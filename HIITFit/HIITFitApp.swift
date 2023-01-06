2a3c823457895ba5b20d29ee6ead8a46bbb4fddf
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-01-06 16:16:32 +0900

    [Build] Add and save history logics.

--- HIITFit/HIITFitApp.swift
+++ HIITFit/HIITFitApp.swift
@@ -9,10 +9,28 @@ import SwiftUI
 
 @main
 struct HIITFitApp: App {
+    @State private var showAlert = false
+    @StateObject private var historyStore: HistoryStore
+    init() {
+        let historyStore: HistoryStore
+        do {
+            historyStore = try .init(withChecking: true)
+        } catch {
+            print("Could not load history data")
+            historyStore = .init()
+            showAlert = true
+        }
+        _historyStore = .init(wrappedValue: historyStore)
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .environmentObject(HistoryStore())
+                .environmentObject(historyStore)
+                .alert(isPresented: $showAlert) {
+                    .init(title: .init("History"), message: .init("""
+Unfortunately we can't load your history.
+"""))
+                }
         }
     }
 }
--- HIITFit/Models/HistoryStore.swift
+++ HIITFit/Models/HistoryStore.swift
@@ -7,6 +7,12 @@
 
 import Foundation
 
+enum FileError: Error {
+    case loadFailure
+    case saveFailure
+    case urlFailure
+}
+
 struct ExerciseDay: Identifiable {
     let id: UUID = .init()
     let date: Date
@@ -15,10 +21,18 @@ struct ExerciseDay: Identifiable {
 
 class HistoryStore: ObservableObject {
     @Published var exerciseDays: [ExerciseDay] = []
+    init(withChecking: Bool) throws {
+        //#if DEBUG
+        //        createDevelopmentData()
+        //#endif
+        do {
+            try load()
+        } catch {
+            throw error
+        }
+    }
     init() {
-//#if DEBUG
-//        createDevelopmentData()
-//#endif
+
     }
     /// Add a finished exercise into the store.
     ///   - parameters:
@@ -31,5 +45,38 @@ class HistoryStore: ObservableObject {
         } else {
             exerciseDays.insert(ExerciseDay(date: today, exercises: [exerciseName]), at: 0)
         }
+        do {
+            try save()
+        } catch {
+            fatalError(error.localizedDescription)
+        }
+    }
+    func load() throws {
+
+    }
+    func save() throws {
+        guard let dataURL = getURL() else {
+            throw FileError.urlFailure
+        }
+        let result: (ExerciseDay) -> [Any] = { exerciseDay in
+            return [
+                exerciseDay.id.uuidString,
+                exerciseDay.date,
+                exerciseDay.exercises
+            ]
+        }
+        let plistData = exerciseDays.map(result)
+        do {
+            let data = try PropertyListSerialization.data(fromPropertyList: plistData, format: .binary, options: .zero)
+            try data.write(to: dataURL, options: .atomic)
+        } catch {
+            throw FileError.saveFailure
+        }
+    }
+    func getURL() -> URL? {
+        guard let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else {
+            return nil
+        }
+        return documentsURL.appendingPathComponent("history.plist")
     }
 }
